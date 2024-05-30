5c773428d7b6fc8154d8c3341f92ca242558a1fe
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-05-30 09:55:03 +0300

    feat: add burger menu

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const BODY = document.querySelector("body");
+const OVERLAY_CONTENT = document.querySelector(".menu__overlay");
+const header__menu = document.querySelector('.header__menu');
+const button__logIn = document.querySelector('.button--logIn');
+const button__signUp = document.querySelector('.button--signUp');
+const menu__cross = document.querySelector('.menu__cross');
+const container__burger = document.querySelector(".wrapper__burger");
+
+
 const container_range = document.querySelector(".blog__list-wrapper");
 let range = document.querySelector(".range");
 let rangeNum = document.querySelector(".blog__list");
+
 //variables
 let time = 500;
 let count = 0;
@@ -61,4 +71,16 @@ function moveRange(distance, step) {
       rangeNum.style.top = distance + step * 0 + "px";
       break;
   }
+}
+
+// menu
+container__burger.addEventListener('click', addBurgerMenu) 
+function addBurgerMenu() {
+  header__menu.classList.toggle("active__menu");
+    button__logIn.classList.toggle("active__menu");
+    button__signUp.classList.toggle("active__menu");
+  menu__cross.classList.toggle("active__menu");
+  BODY.classList.toggle("lock");
+  OVERLAY_CONTENT.classList.toggle("active__menu");
+  console.log('hi');
 }

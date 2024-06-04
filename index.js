a4ae9a22cce648e142166dab028de873853f6d6b
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-06-04 18:05:02 +0300

    fix: change function scroll for range

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const button__logIn = document.querySelector('.button--logIn');
 const button__signUp = document.querySelector('.button--signUp');
 const menu__cross = document.querySelector('.menu__cross');
 const container__burger = document.querySelector(".wrapper__burger");
-const blog__items  = document.querySelectorAll(".blog__item ");
+const blog__items = document.querySelectorAll(".blog__item ");
 
 
 const container_range = document.querySelector(".blog__list-wrapper");
-let range = document.querySelector(".range");
-let rangeNum = document.querySelector(".blog__list");
+const SCROLL = document.querySelector('.range');
+const reviewsList = document.querySelector('.blog__list');
 
 //variables
 let time = 500;
@@ -20,86 +20,17 @@ let stepOffset = 162;
 //variables
 
 
-function addActive (item) {
-  blog__items.forEach((i)=>{
-    i.classList.remove("active");
-  })
-  setTimeout(()=>{
-    blog__items[item].classList.add("active");
-  },500)
-}
+const scrollReviews = function (value) {
+  reviewsList.style.transform = "translateY(0px)";
 
-function transDown() {
-  var animation = rangeNum.animate(
-    [{ transform: `translateY(-${stepOffset}px)` }, { transform: "translateY(0)" }],
-    time
-  );
-  animation.addEventListener("finish", function () {
-    rangeNum.style.transform = "translateY(0)";
-  });
-}
-function transTop() {
-  var animation = rangeNum.animate(
-    [{ transform: `translateY(${stepOffset}px)` }, { transform: "translateY(0)" }],
-    time
-  );
-  animation.addEventListener("finish", function () {
-    rangeNum.style.transform = "translateY(0)";
-  });
-}
+  let step = window.innerWidth < 600 ? stepOffset = 112 : stepOffset = 162;
+  let trans = value * step;
 
-range.addEventListener("input", () => {
-  const windowInnerWidth = window.innerWidth;
-  windowInnerWidth < 600 ? stepOffset = 112: stepOffset = 162
-  windowInnerWidth < 600 ? offsetBottom = -336: offsetBottom = -486
-  moveRange(offsetBottom, stepOffset);
+  reviewsList.style.transform = `translateY(-${trans}px)`;
+};
+SCROLL.addEventListener("click", () => {
+  scrollReviews(SCROLL.value);
 });
-
-function moveSide(value) {
-  if (count < value) {
-    transTop();
-    count = value;
-  } else {
-    transDown();
-    count = value;
-  }
-  return;
-}
-
-function moveRange(distance, step) {
-  switch (Number(range.value)) {
-    case 0:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * 3 + "px";
-      addActive(range.value);
-      break;
-    case 1:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * 2 + "px";
-      addActive(range.value);
-      break;
-    case 2:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * 1 + "px";
-      addActive(range.value);
-      break;
-    case 3:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * 0 + "px";
-      addActive(range.value);
-      break;
-    case 4:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * -1 + "px";
-      addActive(range.value);
-      break;
-    case 5:
-      moveSide(range.value);
-      rangeNum.style.top = distance + step * -2 + "px";
-      addActive(range.value);
-      break;
-  }
-}
 
 // menu
 container__burger.addEventListener('click', addBurgerMenu)
